8b19511ab914b167d7afa8534560032b6e0c69c8
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2018-09-01 11:56:21 +0200

    jhipster-mongodb-relations: import-jdl
    node_modules/generator-jhipster/cli/jhipster.js import-jdl ../models-jdl/orders-model.jdl

---
+++ jhipster-mongodb/src/main/webapp/app/entities/ordered-item/ordered-item.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { JhipsterMongodbSharedModule } from 'app/shared';
+import {
+    OrderedItemComponent,
+    OrderedItemDetailComponent,
+    OrderedItemUpdateComponent,
+    OrderedItemDeletePopupComponent,
+    OrderedItemDeleteDialogComponent,
+    orderedItemRoute,
+    orderedItemPopupRoute
+} from './';
+
+const ENTITY_STATES = [...orderedItemRoute, ...orderedItemPopupRoute];
+
+@NgModule({
+    imports: [JhipsterMongodbSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    declarations: [
+        OrderedItemComponent,
+        OrderedItemDetailComponent,
+        OrderedItemUpdateComponent,
+        OrderedItemDeleteDialogComponent,
+        OrderedItemDeletePopupComponent
+    ],
+    entryComponents: [OrderedItemComponent, OrderedItemUpdateComponent, OrderedItemDeleteDialogComponent, OrderedItemDeletePopupComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class JhipsterMongodbOrderedItemModule {}
